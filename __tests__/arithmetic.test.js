bf102026681e2669b8f80d4d1d65a53a6452fe2f
Heather Cherewaty <hr.cherewaty@gmail.com>
2018-12-22 16:28:24 -0800

    adds multiply and divide functionality and tests

--- __tests__/arithmetic.test.js
+++ __tests__/arithmetic.test.js
@@ -1,4 +1,59 @@
 'use strict';
 
+//require faker
+const faker = require('faker');
+//require module for test
 const arithmetic = require('../lib/arithmetic.js');
 
+//assign faker variables
+let randomNumber = faker.random.number();
+let randomNumberTwo = faker.random.number();
+// let randomNumberThree = faker.random.number();
+
+describe('Add function', () => {
+  it('adds two numbers', () => {
+    let sum = arithmetic.add(randomNumber, randomNumberTwo);
+    let expectedVal = randomNumber + randomNumberTwo;
+    expect(sum).toEqual(expectedVal);
+  });
+  it('does not take strings as parameters', () => {
+    let sum = arithmetic.add('meow', 'woof');
+    expect(sum).toBeNull();
+  });
+});
+
+describe('Subtract function', () => {
+  it('subtracts two numbers', () => {
+    let minus = arithmetic.subtract(randomNumber, randomNumberTwo);
+    let expectedVal = randomNumber - randomNumberTwo;
+    expect(minus).toEqual(expectedVal);
+  });
+  it('does not take strings as parameters', () => {
+    let minus = arithmetic.subtract('meow', 'woof');
+    expect(minus).toBeNull();
+  });
+});
+
+describe('Multiply function', () => {
+  it('multiples two numbers', () => {
+    let mult = arithmetic.multiply(randomNumber, randomNumberTwo);
+    let expectedVal = randomNumber * randomNumberTwo;
+    expect(mult).toEqual(expectedVal);
+  });
+  it('does not take strings as parameters', () => {
+    let mult = arithmetic.multiply('woof', 'meow');
+    expect(mult).toBeNull();
+  });
+});
+
+describe('Divide function', () => {
+  it('divides two numbers', () => {
+    let div = arithmetic.divide(randomNumber, randomNumberTwo);
+    let expectedVal = randomNumber / randomNumberTwo;
+    expect(div).toEqual(expectedVal);
+  });
+  it('does not accept string as parameters', () => {
+    let div = arithmetic.divide('woof', 'meow');
+    expect(div).toBeNull();
+  });
+});
--- __tests__/greet.test.js
+++ __tests__/greet.test.js
@@ -4,7 +4,7 @@
 const faker = require('faker');
 
 //Require module for test
-const greeting = require('../lib/greet.js')
+const greeting = require('../lib/greet.js');
 
 //assign Faker variables
 
@@ -13,24 +13,24 @@ let randomNameTwo = faker.name.firstName;
 let randomNumber = faker.random.number;
 
 describe('Greeting', () => {
-    it('requires one parameter', () => {
-        let message = greeting.sayHello();
-        expect(message).toBeNull();
-    });
+  it('requires one parameter', () => {
+    let message = greeting.sayHello();
+    expect(message).toBeNull();
+  });
 
-    it('does not allow more than 1 name', () => {
-        let message = greeting.sayHello(randomNameOne, randomNameTwo);
-        expect(message).toBeNull();
-    });
+  it('does not allow more than 1 name', () => {
+    let message = greeting.sayHello(randomNameOne, randomNameTwo);
+    expect(message).toBeNull();
+  });
 
-    it('does not allow numeric values', () => {
-        let message = greeting.sayHello(randomNumber);
-        expect(message).toBeNull();
-    });
+  it('does not allow numeric values', () => {
+    let message = greeting.sayHello(randomNumber);
+    expect(message).toBeNull();
+  });
 
-    it('passes when given a word', () => {
-        let message = greeting.sayHello('World');
-        let expectedOutput = 'Hello, World!';
-        expect(message).toEqual(expectedOutput);
-    });
+  it('passes when given a word', () => {
+    let message = greeting.sayHello('World');
+    let expectedOutput = 'Hello, World!';
+    expect(message).toEqual(expectedOutput);
+  });
 });
