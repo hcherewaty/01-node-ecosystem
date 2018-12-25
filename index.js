53da4ab87ff8611c409479b78264ca240a47c419
Heather Cherewaty <hr.cherewaty@gmail.com>
2018-12-25 14:58:20 -0800

    adds console.logs on index.js for multiplication and division function tests

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ const math = require('./lib/arithmetic.js');
 console.log(greet.sayHello('JOHN'));
 console.log(math.add(1,3)); // 4
 console.log(math.subtract(1,3)); // -2
+console.log(math.multiply(1,3)); // 3
+console.log(math.divide(9,3)); // 3
